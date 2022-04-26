d643a2c9df960daeb26841b9aec91d6e3aeb878c
Hernán Kleinubing <hernan.k97@gmail.com>
2022-04-26 20:08:33 -0300

    setupReloj
    Cree el proyecto y agregue todos los otros, agrupando por carpetas

---
+++ Reloj/Reloj/Reloj/RelojModule.c
@@ -0,0 +1,18 @@
+/*
+ * Reloj.c
+ *
+ * Created: 4/26/2022 7:54:03 PM
+ * Author : Hernan
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ Reloj/Reloj/Reloj/oscilador/OsciladorModule.c
@@ -0,0 +1,35 @@
+/*
+ * timer0_oscilador10hz.c
+ *
+ * Created: 18/4/2022 05:47:37
+ * Author : Administrator
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+unsigned cont=0;
+ISR(TIMER0_OVF_vect)
+{
+	cont++;
+	if(cont==1){
+		PORTB^=(1<<PORTB5);//invierto PB5		
+		TCNT0=99;//reinicio contador del timer0
+		cont=0;//reinicio contador
+	}
+	
+}
+
+int main(void)
+{
+	DDRB=0b00100000;//PB5 como salida
+	TCCR0B=(1<<CS02)|(1<<CS00);//configurar el registro del timer0 como temporizador con prescalador de 1024
+	TCNT0=99;//el registro empieza con valor 99
+	TIMSK0|= (1<<TOIE0);//habilita la interrupcion por desbordamiento del timer0
+	sei();//habilita interrupciones globales
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
